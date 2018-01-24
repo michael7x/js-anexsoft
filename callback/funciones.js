d241478ef583aa97341dc469ef8576c72fa478c0
Michael Rodríguez Coello <michael7x@hotmail.com>
2018-01-24 12:31:52 -0500

    eliminacion
    de comentarios

--- callback/funciones.js
+++ callback/funciones.js
@@ -5,7 +5,3 @@ function hagoAlgo(callback){
 hagoAlgo(function(){
     console.log("Hola ROCO !!");
 });
-
-// Continuando con la leccion
-
-// OJO se deben dar 2 saltos en VSCODE para que coja el comentario
