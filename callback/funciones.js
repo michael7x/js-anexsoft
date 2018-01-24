a9bb6f954119799c4baf5471c5a73c13202fa801
Michael Rodríguez Coello <michael7x@hotmail.com>
2018-01-24 12:33:15 -0500

    VSCODE
    Se deben dar 2 saltos de linea

--- callback/funciones.js
+++ callback/funciones.js
@@ -5,3 +5,7 @@ function hagoAlgo(callback){
 hagoAlgo(function(){
     console.log("Hola ROCO !!");
 });
+
+
+// Ahora si listo para continuar
+// Recuerda que se deben dar 2 saltos de linea para añadir el comentario en VSCode
